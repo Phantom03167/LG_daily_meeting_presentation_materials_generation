da8e4980296d4bfb30ce163295661da7ea7e9332
Phantom <confusion371@gmail.com>
2024-10-02 16:38:51 +0800

    优化代码；修改BUG

--- 立管日例会汇报材料文本生成.py
+++ 立管日例会汇报材料文本生成.py
@@ -4,9 +4,12 @@ import pandas as pd
 import os, sys
 
 
+RES_TEXT_FILE = os.path.join(os.environ['USERPROFILE'], 'Downloads', '当日汇报材料文本.txt')
+
 def load_current_day_data():
     # 获取当前日期
     current_date = datetime.now().date().strftime("%#m月%#d日")
+    print(current_date)
     
     df = pd.read_excel(r"..\2024年一分公司立管改造日情况统计表.xlsx", sheet_name=current_date, header=[0, 1], skiprows=1)
     # df = pd.read_excel(r"..\2024年一分公司立管改造日情况统计表.xlsx", sheet_name='9月29日', header=[0, 1], skiprows=1)
@@ -29,11 +32,27 @@ def get_format_text(df:pd.DataFrame):
 
     # 前一日完成情况
     # 第一段
-    previous_situation_text += "昨日计划施工6.45公里，昨日实际完成{:.3g}公里，累计完成{:.3g}公里。\n".format(
+    previous_situation_text += "昨日计划施工6.45公里，实际完成立管{:d}串，完成{:.3g}公里，PMS系统内录入工程量{:.3g}公里。累计完成立管{:d}串，累计完成{:.3g}公里，PMS系统内累计录入工程量{:.3g}公里，累计打眼数{:d}个。\n".format(
+        df.at['合计', '当日立管串数'],
         round(df.at['合计', '当日实际完成量'] / 1000, 3),
+        round(df.at['合计', '当日PMS系统录入量'] / 1000, 3),
+        df.at['合计', '累计立管串数'],
         round(df.at['合计', '累计实际完成量'] / 1000, 3),
+        round(df.at['合计', '累计PMS系统录入量'] / 1000, 3),
+        df.at['合计', '累计打眼数量'],
     )
     # 第二段
+    previous_situation_text += "其中，"
+    for row in df.loc['小计'].itertuples():
+        previous_situation_text += "{}昨日立管{:d}串，工程量{:.3g}公里，累计立管{:d}串，累计工程量{:.3g}公里；".format(
+             row.施工队伍 if len(row.施工队伍) >= 4 else row.施工队伍 + '公司',
+             row.当日立管串数,
+             round(row.当日实际完成量 / 1000, 3),
+             row.累计立管串数,
+             round(row.累计实际完成量 / 1000, 3),
+         )
+    previous_situation_text = previous_situation_text[:-1] + "。\n"
+    # 第三段
     previous_situation_text += "昨日"
     for row in df.itertuples():
         if not pd.isna(row.序号):
@@ -44,11 +63,6 @@ def get_format_text(df:pd.DataFrame):
                 round(row.累计实际完成量 / 1000, 3),
             )
     previous_situation_text = previous_situation_text[:-1] + "。"
-    previous_situation_text += "开片小区累计完成立管{:d}串，共计{:.3g}公里。PMS系统内录入工程量{:.3g}公里。".format(
-        df.at['合计', '累计立管串数'],
-        round(df.at['合计', '累计实际完成量'] / 1000, 3),
-        round(df.at['合计', '累计PMS系统录入量'] / 1000, 3),
-    )
 
     # 当日完成情况
     # 表格数据
@@ -56,34 +70,29 @@ def get_format_text(df:pd.DataFrame):
     number_of_holes = df['累计打眼数量'][(df.index != '小计') & (df.index != '合计') & (df.index != '平昌楼')].to_list()
     
     # 第一段
-    current_situation_text += "今日计划进场施工人数{:d}人，实际{:d}人，其中罡世公司{:d}人，中石化建{:d}人，累计打眼数{:d}个。\n".format(
+    current_situation_text += "今日计划进场施工人数{:d}人，实际{:d}人，其中罡世公司{:d}人，中石化建{:d}人。".format(
         df['序号'].count() * 12,
         df.at['合计', '施工人数'],
         df.loc[(df.index == '小计') & (df['施工队伍'] == '罡世'), '施工人数'].values[0],
         df.loc[(df.index == '小计') & (df['施工队伍'] == '中石化建'), '施工人数'].values[0],
-        df.at['合计', '累计打眼数量'],
-    )
-    # 第二段
-    current_situation_text += "罡世公司昨日立管{:d}串，累计立管{:d}串，昨日工程量{:.3g}公里，累计工程量{:.3g}公里。\n".format(
-        df.loc[(df.index == '小计') & (df['施工队伍'] == '罡世'), '当日立管串数'].values[0],
-        df.loc[(df.index == '小计') & (df['施工队伍'] == '罡世'), '累计立管串数'].values[0],
-        round(df.loc[(df.index == '小计') & (df['施工队伍'] == '罡世'), '当日实际完成量'].values[0] / 1000, 3),
-        round(df.loc[(df.index == '小计') & (df['施工队伍'] == '罡世'), '累计实际完成量'].values[0] / 1000, 3),
-    )
-    current_situation_text += "中石化建昨日立管{:d}串，累计立管{:d}串，昨日工程量{:.3g}公里，累计工程量{:.3g}公里。".format(
-        df.loc[(df.index == '小计') & (df['施工队伍'] == '中石化建'), '当日立管串数'].values[0],
-        df.loc[(df.index == '小计') & (df['施工队伍'] == '中石化建'), '累计立管串数'].values[0],
-        round(df.loc[(df.index == '小计') & (df['施工队伍'] == '中石化建'), '当日实际完成量'].values[0] / 1000, 3),
-        round(df.loc[(df.index == '小计') & (df['施工队伍'] == '中石化建'), '累计实际完成量'].values[0] / 1000, 3),
     )
 
     # 输出文本
-    print(previous_situation_text)
-    print("=" * 150)
-    print(' '.join(map(str, number_of_workers)))
-    print(' '.join(map(str, number_of_workers)))
-    print("-" * 100)
-    print(current_situation_text)
+    # print(previous_situation_text)
+    # print("=" * 150)
+    # print('\t'.join(map(str, number_of_workers)))
+    # print('\t'.join(map(str, number_of_holes)))
+    # print("-" * 100)
+    # print(current_situation_text)
+    
+    # 写入文件
+    with open(RES_TEXT_FILE, 'w', encoding='utf-8') as f:
+        f.write(previous_situation_text + '\n')
+        f.write("=" * 150 + '\n')
+        f.write('\t'.join(map(str, number_of_workers)) + '\n')
+        f.write('\t'.join(map(str, number_of_holes)) + '\n')
+        f.write("-" * 100 + '\n')
+        f.write(current_situation_text)
 
 
 if __name__ == "__main__":
