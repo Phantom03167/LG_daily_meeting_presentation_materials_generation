d75fcc673eeb75af217225ddadd27b4b2c3af586
Phantom <confusion371@gmail.com>
2024-10-09 16:03:26 +0800

    汇报内容改版

--- 立管日例会汇报材料文本生成.py
+++ 立管日例会汇报材料文本生成.py
@@ -30,16 +30,15 @@ def get_format_text(df:pd.DataFrame):
     number_of_workers = []
     number_of_holes = []
 
-    # 前一日完成情况
+    # 进度概况
     # 第一段
-    previous_situation_text += "昨日计划施工4公里，实际完成立管{:d}串，完成{}公里，PMS系统内录入工程量{}公里。累计完成立管{:d}串，累计完成{}公里，PMS系统内累计录入工程量{}公里，累计打眼数{:d}个。\n".format(
+    previous_situation_text += "昨日计划施工4公里，实际完成立管{:d}串，完成{}公里，PMS系统内录入工程量{}公里。累计完成立管{:d}串，累计完成{}公里，PMS系统内累计录入工程量{}公里。\n".format(
         df.at['合计', '当日立管串数'],
         round(df.at['合计', '当日实际完成量'] / 1000, 3),
         round(df.at['合计', '当日PMS系统录入量'] / 1000, 3),
         df.at['合计', '累计立管串数'],
         round(df.at['合计', '累计实际完成量'] / 1000, 3),
         round(df.at['合计', '累计PMS系统录入量'] / 1000, 3),
-        df.at['合计', '累计打眼数量'],
     )
     # 第二段
     previous_situation_text += "其中，"
@@ -51,24 +50,8 @@ def get_format_text(df:pd.DataFrame):
              row.累计立管串数,
              round(row.累计实际完成量 / 1000, 3),
          )
-    previous_situation_text = previous_situation_text[:-1] + "。\n"
+    previous_situation_text = previous_situation_text[:-1].replace("0.0公里", "0公里") + "。\n"
     # 第三段
-    previous_situation_text += "昨日"
-    for row in df.itertuples():
-        if not pd.isna(row.序号):
-            previous_situation_text += "{}立管{:d}串、累计完成{:d}串、累计完成{}公里；".format(
-                row.Index,
-                row.当日立管串数,
-                row.累计立管串数,
-                round(row.累计实际完成量 / 1000, 3),
-            )
-    previous_situation_text = previous_situation_text[:-1].replace("0.0公里", "0公里") + "。"
-
-    # 当日完成情况
-    # 表格数据
-    number_of_workers = df['施工人数'][(df.index != '小计') & (df.index != '合计') & (df.index != '平昌楼')].to_list()
-    number_of_holes = df['累计打眼数量'][(df.index != '小计') & (df.index != '合计') & (df.index != '平昌楼')].to_list()
-    # 第一段
     current_situation_text += "今日计划进场施工人数{:d}人，实际{:d}人，其中罡世公司{:d}人，中石化建{:d}人。".format(
         (df['序号'].count() - 1) * 12,
         df.at['合计', '施工人数'],
@@ -78,19 +61,11 @@ def get_format_text(df:pd.DataFrame):
 
     # 输出文本
     # print(previous_situation_text)
-    # print("=" * 150)
-    # print('\t'.join(map(str, number_of_workers)))
-    # print('\t'.join(map(str, number_of_holes)))
-    # print("-" * 100)
     # print(current_situation_text)
     
     # 写入文件
     with open(RES_TEXT_FILE, 'w', encoding='utf-8') as f:
-        f.write(previous_situation_text + '\n')
-        f.write("=" * 150 + '\n')
-        f.write('\t'.join(map(str, number_of_workers)) + '\n')
-        f.write('\t'.join(map(str, number_of_holes)) + '\n')
-        f.write("-" * 100 + '\n')
+        f.write(previous_situation_text)
         f.write(current_situation_text)
 
 
